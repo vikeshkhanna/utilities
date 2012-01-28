39bb00d4e8ca8c0ea7bafe7176e9caddc1e7c58b
Vikesh Khanna <khanna.vikesh@gmail.com>
2012-01-29 04:45:20 +0530

    Adding song title rename feature

--- music/janitor/janitor.py
+++ music/janitor/janitor.py
@@ -1,10 +1,12 @@
 import os
 import sys
 import re
+from mutagen.easyid3 import EasyID3
 
 utility_name = "Janitor"
 user_regex = "";
 
+#root directory
 try:
 	root = sys.argv[1];
 except IndexError:
@@ -14,14 +16,16 @@ except IndexError:
 		print "\n\n" + root + " does not exist. Using current working directory.\n";
 		root = os.getcwd();
 
+#custom regex input
 user_regex = str(raw_input("\n\nYou can specify your own python-style regular expression if you have anything apart from numbers leading every file name, \
 like, say, the artist name. Press enter if you don't understand this part: "));
 
 if user_regex != "":
-	message = "\nWarning! " + utility_name + " will rename all files that meet the criteria recursively from " + root + ". \n0 to Quit, 1 to Rename, 2 to Test Custom Regex: "
+	message = "\nWarning! " + utility_name + " will rename all files that meet the criteria recursively from " + root + ". \n0 to Quit, 1 to Rename Files Only, 2 to Rename Files and Song Title, 3 to Test Custom Regex: "
 else:
-	message = "\nWarning! " + utility_name + " will rename all files that meet the criteria recursively from " + root + ". \n0 to Quit, 1 to Rename: "
-	
+	message = "\nWarning! " + utility_name + " will rename all files that meet the criteria recursively from " + root + ". \n0 to Quit, 1 to Rename Files Only, 2 to Rename Files and Song Title : "
+
+#user decision
 t = int(raw_input(message));
 
 if t == 0:
@@ -30,15 +34,36 @@ if t == 0:
 else:
 	print "\nRunning " + utility_name + "! Please be patient.\n";
 
+#logic
 for dirpath, dirnames, filenames in os.walk(root):
 	for file in filenames:
 			#print re.sub('^(\d+\s*[.]?\s*[-_]?\s*)?\s*(' + user_regex + ')?\s*','', file);
-			new_name = re.sub('^(\d+\s*[.]?\s*[-_]?\s*)?\s*(' + user_regex + ')?\s*','', file);
-			print file + " > " + new_name
+			old_filename = file[0: file.rindex('.')];
+			extension = file[file.rindex('.'):];
 			
-			if t!=2:
+			new_name = re.sub('^(\d+\s*[.]?\s*[-_]?\s*)?\s*(' + user_regex + ')?\s*','', old_filename);
+			new_filename = new_name + extension;
+			
+			print "[Filename] " + file + " > " + new_filename
+			
+			#test custom regex
+			if t != 3:
 				try:
-					os.rename(os.path.join(dirpath, file), os.path.join(dirpath, new_name));
+					os.rename(os.path.join(dirpath, file), os.path.join(dirpath, new_filename));
+					
+					#rename files and ID3
+					if t == 2:
+						try:
+							new_file = os.path.join(dirpath, new_filename);
+							audio = EasyID3(os.path.join(dirpath, new_file));
+							old_title = str(audio["title"])
+							audio["title"] = new_name;
+							audio.save()
+							print "[Title] " + old_title + " > " + str(audio["title"])
+							
+						except Exception as err:
+							print "Could not rename audio title : " + str(err)
+			
 				except Exception as (errno, strerror):
 					print "Could not rename file : " + strerror + "(" + str(errno) +")"
 					
