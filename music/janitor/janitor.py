1d90dd625282a31aba3cfdc30703bd82b9019e4c
Vikesh Khanna <khanna.vikesh@gmail.com>
2012-01-29 02:20:05 +0530

    Adding custom regex feature

--- music/janitor/janitor.py
+++ music/janitor/janitor.py
@@ -3,16 +3,20 @@ import sys
 import re
 
 utility_name = "Janitor"
+user_regex = "";
 
 try:
 	root = sys.argv[1];
 except IndexError:
-	root = raw_input("\nPlease enter the path of the root directory without trailing slash : ");
-
-	if not os.path.exists(root):
-		print "\n" + root + " does not exist. Using current working directory.\n";
-		root = os.getcwd();
+	root = str(raw_input("\nPlease enter the path of the root directory without trailing slash : "));
 	
+	if not os.path.exists(root):
+		print "\n\n" + root + " does not exist. Using current working directory.\n";
+		root = os.getcwd();
+
+user_regex = str(raw_input("\n\nYou can specify you own python-style regular expression if you have anything apart from numbers leading every file name, \
+like, say, the artist name. Press enter if you don't understand this part: "));
+
 t = raw_input("Warning! " + utility_name + " will rename all files recursively from " + root + " beginning with numbers. 0 to quit, 1 to confirm: ");
 
 if int(t) == 0:
@@ -23,8 +27,8 @@ else:
 
 for dirpath, dirnames, filenames in os.walk(root):
 	for file in filenames:
-			#print re.sub('^\d+\s*[.]?\s*','', file);
-			new_name = re.sub('^\d+\s*[.]?\s*','', file);
+			#print re.sub('^(\d+\s*[.]?\s*[-_]?\s*)?\s*(' + user_regex + ')?\s*','', file);
+			new_name = re.sub('^(\d+\s*[.]?\s*[-_]?\s*)?\s*(' + user_regex + ')?\s*','', file);
 			print file + " > " + new_name
 			os.rename(os.path.join(dirpath, file), os.path.join(dirpath, new_name));
 
