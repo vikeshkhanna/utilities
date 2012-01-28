6ecef9ac4eeed87836dab80c2a82abe38b234ee3
unknown <vikesh@vikesh-pc.(none)>
2012-01-29 00:58:02 +0530

    Updating default root directory policy

--- music/janitor/janitor.py
+++ music/janitor/janitor.py
@@ -2,18 +2,24 @@ import os
 import sys
 import re
 
+utility_name = "Janitor"
+
 try:
 	root = sys.argv[1];
 except IndexError:
-	root = os.getcwd();
+	root = raw_input("\nPlease enter the path of the root directory without trailing slash : ");
 
-t = raw_input("Warning! This utility will rename all files recursively from " + root + " beginning with numbers. 0 to quit, 1 to confirm: ");
+	if not os.path.exists(root):
+		print "\n" + root + " does not exist. Using current working directory.\n";
+		root = os.getcwd();
+	
+t = raw_input("Warning! " + utility_name + " will rename all files recursively from " + root + " beginning with numbers. 0 to quit, 1 to confirm: ");
 
 if int(t) == 0:
-	print "Cool! Nothing happened!"
+	print "\nCool! Nothing happened!\n"
 	sys.exit(0);
 else:
-	print "\nRunning utility! Please be patient.\n";
+	print "\nRunning " + utility_name + "! Please be patient.\n";
 
 for dirpath, dirnames, filenames in os.walk(root):
 	for file in filenames:
@@ -21,3 +27,5 @@ for dirpath, dirnames, filenames in os.walk(root):
 			new_name = re.sub('^\d+\s*[.]?\s*','', file);
 			print file + " > " + new_name
 			os.rename(os.path.join(dirpath, file), os.path.join(dirpath, new_name));
+
+raw_input('Press enter to quit...');
