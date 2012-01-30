799b4be59e475affde1cdad26185db25270de964
Vikesh Khanna <khanna.vikesh@gmail.com>
2012-01-30 06:24:51 +0530

    Updating Lexy

--- music/lexy/lexy.py
+++ music/lexy/lexy.py
@@ -4,15 +4,19 @@
 
 # Lexy is a word play on Sexy and Lexicon :)
 
-
 import urllib, urllib2
-import sys
+import sys, os
 import re, htmlentitydefs
 from BeautifulSoup import BeautifulSoup
+
 from mutagen.id3 import ID3
+from mutagen.easyid3 import EasyID3
 from mutagen import *
 
 import re, htmlentitydefs
+
+#max number of tries to get lyrics
+MAX_ATTEMPTS = 4
 
 ## Thanks to Fredrik Lundh - http://effbot.org/zone/re-sub.htm#unescape-html
 # Removes HTML or XML character references and entities from a text string.
@@ -56,10 +60,14 @@ def extract_lyrics(html):
 	#erase all comments - this is necessary even if DOM does not have comments because HTML parser will append template limit comment
 	text = re.sub('<\s*!--.*--\s*>','', text, flags = re.DOTALL)
 	
-	return unescape(text)
+	text = unescape(text)
+	
+	if text != "None" :
+		return text
+	else:
+		return None
 	
 def get_lyrics(title):
-	title = "Wherever I may Roam"
 	params = urllib.urlencode({'sourceid': 'navclient', 'btnI': 1, 'q': 'site:lyrics.wikia.com ' + title})
 	url = "http://google.com/search?%s" % params;
 	user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
@@ -95,7 +103,7 @@ if opt == 0:
 final = False
 
 while not final:
-	print "\nWarning! Lexy will change the 'lyrics' ID3 tag of all songs recursively under " + root + " if you choose to add lyrics.\n \
+	print "\nWarning! Lexy will change the 'lyrics' ID3 tag of all songs recursively under " + root + " if you choose to add lyrics.\n\
 0: Quit\n1: Add lyrics to songs\n2: Show fetched lyrics (Test Lexy before modifying songs)"
 	
 	t = int(raw_input("\nWhat would you like to do?: "));
@@ -108,39 +116,75 @@ while not final:
 		
 	for dirpath, dirnames, filenames in os.walk(root):
 		for file in filenames:	
-			
-			replace = False
+			fetch = False
 			
 			try:
-				audio = ID3(os.path.join(dirpath, file));
 				easy_audio = EasyID3(os.path.join(dirpath, file))
-				
-				#okay to throw exception if no title is found
-				title = ' '.join(easy_audio["title"])
-				
-				try: 
-					title = title + ' '.easy_audio["artist"]
-				except KeyError:
-					print "[Warning] Artist name was not found for " + title + ". Lyrics may be wrong."
-				
-				lyrics = get_lyrics(title)
-				print "\n" + title + " : \n" + lyrics[:len(lyrics)/10] + "...(continued) "
 				
 				#fetch missing songs only
 				if opt == 1:
-					if not audio["lyrics"] or len(audio["lyrics"]) == 0:
-						replace = True
+					if "lyrics" in	audio:
+							if len(audio["lyrics"]) > 0:
+								fetch = True
 				#fetch all songs
 				elif opt == 2:
-					replace = True
+					fetch = True
 				
-				if replace:
-					audio[u"USTL::'eng'"] = USLT()
-					audio[u"USTL::'eng'"].text = lyrics
-					audio[u"USTL::'eng'"].encoding = 0
-					audio[u"USTL::'eng'"].lang = 'eng'
-			except Exception as (errno, strerror):
-				print "[error] Could not add lyrics : " + strerror + "(" + str(errno) +")"
-		
+				#okay to throw exception if no title is found
+				title = ' '.join(easy_audio["title"])
+				artist = ''
+				
+				if fetch:
+					try: 
+						artist = ' '.join(easy_audio["artist"])
+					except KeyError:
+						print "\n[Warning] Artist name was not found for " + title + ". Lyrics may be wrong."
+					
+					easy_audio.save()
+					
+					attempts = 0
+
+					while attempts < MAX_ATTEMPTS:
+						print "\nFetching lyrics for " + title + ' ' + artist + ". Please wait."
+						#lyrics = "ada";
+						lyrics = get_lyrics(artist + ' ' + title)
+						
+						if lyrics and len(lyrics)>0:
+							print "\n" + title + " : \n" + lyrics[:len(lyrics)/5] + "...(continued) "
+							break
+						else:
+							message = "\n[Attempt " + str(attempts) + "] Lyrics not found. "
+							
+							if attempts == 0:
+								message +=  "Cleaning leading numbers in title and retrying"
+								title = re.sub("^([(]?\d+[)]?\s*[.]?\s*[-_]?\s*)?\s*","", title)
+							elif attempts == 1:
+								message += "Trying without artist name"
+								artist = ''
+							elif attempts == 2:
+								message += "Cleaning all numbers from the title and retrying"
+								title = re.sub("\d+",'',title)
+							elif attempts == 3:
+								message += "Cleaning trailing braced words and retrying"
+								title = re.sub("\s*\((.\s*)*\)\s*$", '', title)
+							print message
+						attempts += 1
+						
+					try:
+						audio = ID3(os.path.join(dirpath, file));
+				
+						if lyrics and len(lyrics)>0 and final:
+							audio[u"USLT::'eng'"] = id3.USLT()
+							audio[u"USLT::'eng'"].text = lyrics
+							audio[u"USLT::'eng'"].encoding = 0
+							audio[u"USLT::'eng'"].lang = 'eng'
+							audio.save()
+							print "Successfully added lyrics for " + title
+							
+					except Exception as err:
+							print "\n[error] Could not add lyrics: " + str(err)
+							
+			except Exception as err:	
+				print "\n[error] " + str(err)
 			
 	
